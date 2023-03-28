1e0893af4bbcc3b5dd0b49871ff7bd5fe85f97c9
ZUITOPIA <korhwngml123@naver.com>
2023-03-28 17:24:49 +0900

    Add BAEKJOON 9012

---
+++ python/python-study/9012.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for i in range(n):
+  strArr = list(input())
+  sum = 0
+
+  for i in strArr:
+    if i == "(":
+      sum += 1
+    elif i == ")":
+      sum -= 1
+    if sum < 0:
+      print("NO")
+      break
+
+  if sum > 0:
+    print("NO")
+  elif sum == 0:
+    print("YES")
