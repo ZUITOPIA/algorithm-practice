5960a55eac291c1848ea9131621906c2b6224886
ZUITOPIA <korhwngml123@naver.com>
2023-04-01 20:36:53 +0900

    Update 0323

--- python/lecture/0323.py
+++ python/lecture/0323.py
@@ -1,16 +1,83 @@
-# 알고리즘의 가장 기본이 되는 방법 - 억지기법 : 문제의 정의를 바탕으로 한 가장 직접적인 방법
-# 억지 기법 (brute-force method, naive method) 예 : 최대 공약수 방법, a^n 구하는 알고리즘 등
+# 선택 정렬 : O(n^2)
+def selection_sort(A):
+    n = len(A)
+    for i in range(n-1):
+        least = i
+        for j in range(i+1, n):
+            if (A[j] < A[least]):
+                least = j
+        A[i], A[least] = A[least], A[i]
+        printStep(A, i+1)
 
-# 탐색 알고리즘에서 억지 기법을 사용하는 것 : 완전 탐색 (억지 기법의 가장 대표적인 예)
+# 선택 정렬 알고리즘 테스트
+data = [5, 3, 8, 4, 9, 1, 6, 2, 7]
+print("Original : ", data)
+selection_sort(data)
+print("Selection: ", data)
 
-# 억지 기법의 중요성
-# 해결하지 못하는 것보다 단순하게라도 해결하는 것이 훨씬 좋고, 쉬운 문제를 어렵게 풀 필요도 없음
-# 매우 광범위한 문제에 적용할 수 있는 알고리즘 설계 기법
-# 입력의 크기가 작은 경우 속도가 충분히 빠름
-# 더 효율적인 알고리즘의 설계와 분석을 위한 이론적인 기반이 됨
+# 순차 탐색 : 최선 O(1) / 최악 O(n) / 평균 O(n)
+def sequential_search(A, key):
+    for i in range(len(A)):
+        if A[i] == key:
+            return i
+        return -1
+    
+# 억지 기법
+# 1. 문자열 매칭 : 최선 O(m) / 최악 O(mn)
+# n : 리스트 길이 , m : 찾고자하는 패턴의 길이
+def string_matching(T, P):
+    n = len(T)
+    m = len(P)
+    for i in range(n-m+1):
+        j = 0
+        while j < m and P[j] == T[i+j]:
+            j = j + 1
+        if j == m:
+            return i
+    return -1
 
-# 첫 번째 억지기법 - 선택 정렬
-# 최소값을 하나씩 뽑아서 새로 정렬할 때의 단점 : 추가적인 리스트가 필요함 (공간효율성 측면에서 추가적인 저장공간이 필요하지 않도록 구현하는 게 중요)
-# 제자리(in-place) 정렬 : 최솟값을 새로운 리스트에 저장하는 것이 아닌 첫번째 요소와 교환하는 방법
+# 2. 최근접 점의 쌍의 거리 : O(n^2)
+def closets_pair(p):
+    n = len(p)
+    mindist = float("inf")
+    for i in range(n-1):
+        for j in range(i+1, n):
+            dist = distance(p[i], p[j])
+            if dist < mindist:
+                mindist = dist
+    return mindist
 
-# 두 번째 억지기법 - 선택 정렬
+# 완전 탐색
+# -> 상태공간트리의 모든 단말 노드를 검사하여 길이가 최소인 것을 선택하는 전략
+# 예 ) 틱택토 게임, 
+# 외판원 문제(TSP 완전탐색) - 그래프가 n개의 정점을 갖는 완전 그래프라면 모든 해밀토니안 사이클은 (n-1)!개 : O(n!)
+# 배낭 채우기 문제 : O(2^n)
+# 일 배정 문제 : O(n!)
+
+# 그래프의 순회 BFS / DFS
+# n (정점의 수), e (간선의 수)
+# 인접 리스트 표현 : O(n+e)
+# 인접 행렬 표현 : O(n^2)
+
+# BFS
+import queue
+def bfs(graph, start):
+    visited = { start }
+    que = queue.Queue()
+    que.put(start)
+    while not que.empty():
+        v = que.get()
+        print(v, end=" ")
+        nbr = graph[v] - visited
+        for u in nbr:
+            visited.add(u)
+            que.put(u)
+
+# DFS
+def dfs(graph, start, visited):
+    if start not in visited:
+        visited.add(start)
+        print(start, end=" ")
+        nbr = graph[start] - visited
+        for v in nbr:
+            dfs(graph, v, visited)
